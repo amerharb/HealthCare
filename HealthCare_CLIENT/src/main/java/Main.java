1e77bcb927f4ce9fb0c4bf2f081d0adc09d416a5
Amer Harb <amer.harb@gmail.com>
2020-10-23 10:27:20 +0200

    add scner to read values

--- HealthCare_CLIENT/src/main/java/Main.java
+++ HealthCare_CLIENT/src/main/java/Main.java
@@ -2,13 +2,34 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
-
         IHealthCare myHealthCare = (IHealthCare) Naming.lookup("rmi://localhost:1099/Recommender");
 
-        int diet = myHealthCare.diet(40,170, 100, "F");
+        System.out.println("connection estabtioned to Health Care Server");
+
+        Scanner scanner = new Scanner(System.in);
+        int age = readNumber("enter age:");
+        int height = readNumber("enter height (cm):");
+        int weight = readNumber("enter weight (kg):");
+
+        int diet = myHealthCare.diet(age,height, weight, "M");
         System.out.println("diet is: " + diet);
+    }
+
+    private static int readNumber(String msg) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            try {
+                System.out.println(msg);
+                String line = scanner.nextLine();
+                if (line.equalsIgnoreCase("exit")) System.exit(0);
+                return Integer.parseInt(line);
+            } catch (Exception exception) {
+                System.out.println("invalid try again");
+            }
+        } while(true);
     }
 }
