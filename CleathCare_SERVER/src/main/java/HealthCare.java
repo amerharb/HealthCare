6f3c9def85b75f2af81e08908bacc54e6ffac9dc
Amer Harb <amer.harb@gmail.com>
2020-10-23 09:52:24 +0200

    add SERVER

---
+++ CleathCare_SERVER/src/main/java/HealthCare.java
@@ -0,0 +1,41 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HealthCare extends UnicastRemoteObject implements IHealthCare{
+    protected HealthCare() throws RemoteException {
+       super();
+    }
+
+    public int diet(int age, int height, int weight, String gender) throws RemoteException {
+        int result;
+        if (age < 20) {
+            result = 600;
+        } else {
+            result = 600 - (int) ( 3 * bmi(weight, height));
+        }
+        if (gender.equals("M")) result += 100;
+
+        return result;
+    }
+
+    public int Exercise(int age, int height, int weight, String gender) throws RemoteException {
+        int result;
+        if (age < 30) {
+            result = 45;
+        } else {
+            result = 35 + (int) ( 2.3 * bmi(weight, height));
+        }
+        if (gender.equals("F")) result += 5;
+
+        return result;
+    }
+
+    public boolean Vitamins(int age, String gender) throws RemoteException {
+        return gender.equals("F") || age > 40;
+    }
+
+    private double bmi(int weight, int height){
+        double heightByMeter = (double) height / 100;
+        return weight / (heightByMeter * heightByMeter);
+    }
+}
---
+++ CleathCare_SERVER/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Main {
+    public static void main(String [] args) throws RemoteException{
+
+        HealthCare f = new HealthCare();
+        Registry registry = LocateRegistry.createRegistry(1099);
+        registry.rebind("Recommender", f);
+    }
+}
